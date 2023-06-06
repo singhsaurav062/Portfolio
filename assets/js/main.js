ed0133873e68e8847a5227c533a821e42d8ddfb4
Saurav Singh <singhsaurav062@gmail.com>
2023-06-06 16:12:01 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,3 +192,29 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      project: document.getElementById("project").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_u1e5vdq";
+    const templateID = "template_uzy2dyy";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
